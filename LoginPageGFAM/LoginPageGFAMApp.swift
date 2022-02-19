abd8471c1878f7e52a0ee3b71353285c06fd2131
Santhosh2141 <snthshsrinivas@gmail.com>
2022-02-19 13:51:49 +0530

    Initial Commit

---
+++ LoginPageGFAM/LoginPageGFAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageGFAMApp.swift
+//  LoginPageGFAM
+//
+//  Created by Santhosh Srinivas on 19/02/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageGFAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
